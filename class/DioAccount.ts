352f46cc754ef1f5371dd4e3b61c1f5126a7deb3
Ariel Sousa <contatoarieldzn@gmail.com>
2024-12-31 00:28:58 -0300

    refactor: make all account attributes private and readonly

--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -1,7 +1,7 @@
 export abstract class DioAccount {
-  private name: string
+  private readonly name: string
   private readonly accountNumber: number
-  balance: number = 0
+  protected balance: number = 0
   private status: boolean = true
 
   constructor(name: string, accountNumber: number){
@@ -9,10 +9,6 @@ export abstract class DioAccount {
     this.accountNumber = accountNumber
   }
 
-  setName = (name: string): void => {
-    this.name = name
-    console.log('Nome alterado com sucesso!')
-  }
 
   getName = (): string => {
     return this.name
